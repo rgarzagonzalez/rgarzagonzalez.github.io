a2e34b7118254c36a61bffc40c95f2047c621462
Ricardo Garza <ricardo@madhouse.com.mx>
2020-07-04 18:03:07 -0500

    Convierte de cartesiano a isométrico.

--- js/game.js
+++ js/game.js
@@ -1,37 +1,66 @@
 window.onload = function() {
-	let canvas = document.getElementById('gameCanvas');
-	let c = canvas.getContext('2d');
+	let canvasCart = document.getElementById('gameCanvasCart');
+	let cCart = canvasCart.getContext('2d');
 
-	let tileWidth = 10;
-	let tileHeight = 10;
+	let canvasIso = document.getElementById('gameCanvasIso');
+	let cIso = canvasIso.getContext('2d');
+
+	let greenTile = new Image();
+	greenTile.src = 'img/greenTile.png';
+
+	let redTile = new Image();
+	redTile.src = 'img/redTile.png';
+
+	let tileWidth = greenTile.width;
+	let tileHeight = greenTile.height;
 
 	let gridWidth = 5;
 	let gridHeight = 5;
 
 	let grid = [
 		[1, 1, 1, 1, 1],
-		[1, 0, 0, 0, 1],
-		[1, 0, 0, 0, 1],
+		[1, 0, 1, 0, 1],
+		[1, 1, 0, 1, 1],
 		[1, 0, 0, 0, 1],
 		[1, 1, 1, 1, 1]
 	];
 
+	let xOffset = (canvasIso.width / 2) - (tileWidth / 2);
+	let yOffset = 0;
+
 	function drawGrid() {
 		for(let i = 0; i < gridHeight; i++) {
 			for(let j = 0; j < gridWidth; j++) {
-				placeTile(grid[i][j], [i], [j]);
+				placeTile(grid[i][j], j, i);
 			}
 		}
 	}
 
-	function placeTile(tileKind, y, x) {
-		let posY = y * tileHeight;
-		let posX = x * tileWidth;
+	function placeTile(tileKind, x, y) {
+		let posYCart = y * tileHeight;
+		let posXCart = x * tileHeight;
 
-		c.fillStyle = (tileKind == 0) ? '#00FF00' : '#FF0000';
+		let cordsIso = cartToIso(x, y);
 
-		c.fillRect(posX, posY, tileWidth, tileHeight);
+		//place cart tile
+		cCart.fillStyle = (tileKind == 0) ? '#00FF00' : '#FF0000';
+		cCart.fillRect(posXCart, posYCart, tileHeight, tileHeight);
 
+		//Place Iso tile
+		let tile = (tileKind == 0) ? greenTile : redTile;
+		cIso.drawImage(tile, cordsIso[0], cordsIso[1], tileWidth, tileHeight);
+		
+	}
+
+	function cartToIso(x, y) {
+		let cordsIso = [];
+
+		let posX = ((x - y) * (tileWidth / 2)) + xOffset;
+		let posY = ((x + y) * (tileHeight / 2)) + yOffset;
+
+		cordsIso.push(posX, posY);
+
+		return cordsIso;
 	}
 
 	drawGrid();
